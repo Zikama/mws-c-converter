ca55139fe0373bdba68571942a9d9e1f40cfa84d
Nehemie <35760860+Zikama@users.noreply.github.com>
2018-07-01 18:52:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const public = 'c-current-v5',
+const public = 'c-current-v1',
      // api=`https://free.currencyconverterapi.com/api/v5/convert?q=${identifyMe}&compact=y&callback=?`,
 	  NamesOfChes =[public],allPub=[
-        '/','./index.html','./script/vendor/jquery.js','./script/script.js','./script/numeral.min.js','./style/style.css']
+        '/','/index.html','/script/vendor/jquery.js','/script/script.js','/script/numeral.min.js','/style/style.css']
 ;
 self.addEventListener('install', (event)=> {
 event.waitUntil(
@@ -20,15 +20,15 @@ event.waitUntil(
 self.addEventListener('fetch',(event)=> {
         let requestUrl = new URL(event.request.url);
         if (requestUrl.origin === location.origin) {
-        if (requestUrl.pathname === '/') {event.respondWith(caches.match('./index.html'));return;	
+        if (requestUrl.pathname === '/') {event.respondWith(caches.match('/index.html'));return;	
 }/*
 		if(requestUrl.pathname === api){
-			vent.respondWith(caches.match('./index.html'));
+			event.respondWith(caches.match('./coverter'));
 		return;}*/
 		} 
+
+});
 event.respondWith(
        caches.match(event.request).then((response)=> {
        return response || fetch(event.request);
    }));
-});
-
